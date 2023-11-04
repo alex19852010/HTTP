480d92cf1e071215de4889c035f8ce049610e184
Alex Bogushenko <bogushenkoas@ukr.net>
2023-11-04 14:13:00 +0300

    1_finish

--- main.cpp
+++ main.cpp
@@ -6,5 +6,46 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Alex !!!" << endl;
+    string command;
+
+    while(command != "exit")
+    {
+        cout << "enter command: ";
+        cin >> command;
+
+        if(command == "get")
+        {
+        cpr :: Response r = cpr :: Get(cpr :: Url("http://httpbin.org/get"));
+        cout << r.text << endl;
+        }
+        
+
+        else if(command == "post")
+        {
+         cpr :: Response r = cpr :: Post(cpr :: Url("http://httpbin.org/post"));
+         cout << r.text << endl;
+        }
+
+        else if(command == "put")
+        {
+          cpr :: Response r = cpr :: Put(cpr :: Url("http://httpbin.org/put"));
+          cout << r.text << endl;  
+        }
+
+        else if(command == "delete")
+        {
+          cpr :: Response r = cpr :: Delete(cpr :: Url("http://httpbin.org/delete"));
+          cout << r.text << endl;  
+        }
+       
+        else if(command == "patch")
+        {
+          cpr :: Response r = cpr :: Patch(cpr :: Url("http://httpbin.org/patch"));
+          cout << r.text << endl;  
+        }
+       
+    }
+    cout << "input error" << endl;
+
+    return 0;
 }
